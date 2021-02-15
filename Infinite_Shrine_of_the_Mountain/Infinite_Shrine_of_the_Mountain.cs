4e33b8e45518f6ca5109d80f1533254d6d887115
OkIGotIt <okigotit2018@gmail.com>
2021-02-14 17:28:22 -0700

    Add project files.

---
+++ Infinite_Shrine_of_the_Mountain/Infinite_Shrine_of_the_Mountain.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using System.Security;
+using System.Security.Permissions;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace MyUserName
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.OkIGotIt.Infinite_Shrine_of_the_Mountain", "Infinite Shrine of the Mountain", "1.0.0")]
+    public class Infinite_Shrine_of_the_Mountain : BaseUnityPlugin
+    {
+        public static ConfigEntry<int> maxpurchase { get; set; }
+        public static ConfigEntry<float> delaytime { get; set; }
+        public void Awake()
+        {
+            maxpurchase = Config.Bind<int>("Shrine of the Mountain", "Max uses", 2147483647, "Changes how many times you can use Shrine of the Mountain");
+            delaytime = Config.Bind<float>("Shrine of the Mountain", "Delay time", 2, "Changes the amount of time between Shrine of the Mountain uses");
+            if (maxpurchase.Value < 0 || maxpurchase.Value > 2147483647)
+                maxpurchase.Value = 2147483647;
+            if (delaytime.Value < 0 || delaytime.Value > 2147483647)
+                delaytime.Value = 2;
+            On.RoR2.PurchaseInteraction.OnInteractionBegin += PurchaseInteraction_OnInteractionBegin;
+        }
+
+        private void PurchaseInteraction_OnInteractionBegin(On.RoR2.PurchaseInteraction.orig_OnInteractionBegin orig, PurchaseInteraction self, Interactor activator)
+        {
+            orig(self, activator);
+            var bossBehavior = self.GetComponent<ShrineBossBehavior>();
+            bossBehavior.maxPurchaseCount = maxpurchase.Value;
+            bossBehavior.refreshTimer = delaytime.Value;
+        }
+    }
+}
